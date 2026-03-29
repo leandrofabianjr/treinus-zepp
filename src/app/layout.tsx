5027ce9eb579720745e6a00b26c6a32032705703
Leandro Fabian Jr. <leandrofabianjr@gmail.com>
2026-03-28 21:29:09 -0300

    Atualizando imagens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,42 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Treinus - Zepp',
-  description: 'Dashboard de Treinos',
+  description:
+    'Sincronize seus treinos da Treinus com relógios Amazfit de forma automática.',
+  keywords: ['Treinus', 'Zepp', 'Amazfit', 'Corrida', 'Treino Estruturado'],
+  authors: [{ name: 'Leandro Fabian Jr.' }],
+
+  openGraph: {
+    title: 'Treinus - Zepp',
+    description:
+      'Converta sua planilha Treinus em treinos estruturados para o app Zepp.',
+    url: 'https://treinus-zepp.vercel.app',
+    siteName: 'Treinus Zepp Sync',
+    images: [
+      {
+        url: '/logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'Treinus - Zepp',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+
+  // Twitter
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Treinus - Zepp',
+    description: 'Sincronize sua planilha Treinus com seu Amazfit em segundos.',
+    images: ['/logo.png'],
+  },
+
+  // Ícones do Navegador
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/logo.png',
+  },
 };
 
 export default async function RootLayout({
